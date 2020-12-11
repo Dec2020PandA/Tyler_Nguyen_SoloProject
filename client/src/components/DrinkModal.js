91bd788a6837f46fa20e64cabbd7cbc09abdd615
tnguyen28 <tntt_tyler@me.com>
2020-12-10 20:22:51 -0600

    DrinkModal.js holds entire component for view now

--- client/src/components/DrinkModal.js
+++ client/src/components/DrinkModal.js
@@ -1,23 +1,54 @@
 import React, { useState } from "react";
-
-import GButton from "./GButton";
+import axios from "axios";
+import SubmitButton from "./SubmitButton";
 import Modal from "@material-ui/core/Modal";
+import Grid from "@material-ui/core/Grid";
+
+import { navigate } from "@reach/router";
 
 function DrinkModal(props) {
-  const [open, setOpen] = useState(props.open);
+  const { drink } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const createDrink = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/api/drink/create", { drink })
+      .then((res) => {
+        console.log(drink);
+        navigate("/dashboard");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleOpen = () => {
+    console.log("open modal");
+    setIsOpen(true);
+  };
 
   const handleClose = () => {
-    setOpen(false);
+    console.log("close modal");
+    setIsOpen(false);
   };
 
   return (
-    <Modal open={open} onClose={handleClose}>
-      <div>
-        <img src={props.results.strDrinkThumb + "/preview"} alt={"blue"} />
-        {props.results.strDrink}
+    <div>
+      <div onClick={handleOpen}>
+        <Grid container direction="column" style={{ width: 200 }}>
+          <img src={drink.strDrinkThumb + "/preview"} alt={"blue"} />
+          {drink.strDrink}
+        </Grid>
       </div>
-      <GButton buttonTitle="Add to favorites" buttonColor="secondary" />
-    </Modal>
+      <Modal open={isOpen} onClose={handleClose}>
+        <form onSubmit={createDrink}>
+          <img src={drink.strDrinkThumb + "/preview"} alt={"blue"} />
+          {drink.strDrink}
+          <SubmitButton buttonTitle="Add to favorite" buttonColor="primary" />
+        </form>
+      </Modal>
+    </div>
   );
 }
 
